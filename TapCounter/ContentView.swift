82ba7f94f4afc3a6db73f585b42c01ac0acd6e6f
Tetsuo Kawakami <yadorogi47@gmail.com>
2024-01-16 06:42:51 +0900

    Initial Commit

---
+++ TapCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TapCounter
+//
+//  Created by Tetsuo Kawakami on 2024/01/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TapCounter/TapCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapCounterApp.swift
+//  TapCounter
+//
+//  Created by Tetsuo Kawakami on 2024/01/16.
+//
+
+import SwiftUI
+
+@main
+struct TapCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
